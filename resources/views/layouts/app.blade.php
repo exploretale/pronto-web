88d11a6a6f01bd2de981a7f64dc9286edbb5c61b
Ivan Bergonia <ivan.bergonia@coreproc.ph>
2017-12-02 16:24:48 +0800

    Update Home and Login template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,21 +30,17 @@
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
+                        <em style="font-size: 0.6em">Merchant Login</em>
                     </a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
